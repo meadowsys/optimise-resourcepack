07f93898d97bafc781c52a9a15d1985adbe88e90
Alexander Liu <liujalexander84@gmail.com>
2020-10-30 19:15:26 -0700

    better readme and docs maybe??????????????

--- README.md
+++ README.md
@@ -11,8 +11,20 @@ if you have npm installed (you should, it comes with node by default), you can r
 1. `git clone` and `cd` into the dir (standard stuff)
 2. `pnpm i` to install dependencies (still standard (npm) stuff)
 3. put your resourcepack files inside a folder called `original` (not the zip file, put the loose files)
-4. `pnpm run optimise` to optimise/compress your resourcepack
-5. `pnpm run optimise:verbose` to run with verbose output
-6. `pnpx gulp --tasks` to list available tasks
-7. `pnpx gulp <taskname>` to run a task (where `<taskname>` is the name of the task)
-8. see the gulpfile (`gulpfile.js`) for more info on what happens
+
+## use
+
+1. `pnpm run optimise` to optimise/compress your resourcepack. the output is put inside `out/optimised`
+2. `pnpm run optimise:verbose` to run with verbose output
+3. `pnpx gulp --tasks` to list available tasks
+4. `pnpx gulp <taskname>` to run a task (where `<taskname>` is the name of the task)
+5. see the gulpfile (`gulpfile.js`) for more info on what happens
+
+### available tasks
+
+- `optimise` task to copy, and optimise if possible, the files for the resourcepack into `out/optimised`. `optimise:verbose` to enable verbose output if available.
+- `copy` to copy the files that would be included inside the copying/optimising task into `out/copy`, without touching them. included for comparison reasons, and just why not lol
+- `compare` to run both the `optimise` and `copy` task, so that you get an uncompressed version and a compress version to compare sizes and make sure nothing went wrong.
+- `png`, `mcmeta`, `json`, `ogg` tasks. for each one, a copy and verbose version are available (for png, the three available ones are `png`, `png:verbose`, and `png:copy`).
+- `copylicense` to copy all license files into the optimised directory (`copylicense:copy` to copy it into the copying directory instead)
+- `clean` to clean and delete the `out` folder. (`clean:copy` and `clean:optimise` available to delete only their respective output directories)
--- gulpfile.js
+++ gulpfile.js
@@ -31,31 +31,52 @@ const mcmeta = (opts) => mcmetasrc().pipe(jsonmin(opts)).pipe(dest(`${destdir}/$
 const ogg = (opts) => oggsrc().pipe(dest(`${destdir}/${optimiseddir}`));
 
 
+// optimise png images into output dir
 task("png", () => png());
+// optimise png images with verbose output into output dir
 task("png:verbose", () => png(verbose));
+// copy png images into copy dir
 task("png:copy", () => pngsrc().pipe(dest(`${destdir}/${copieddir}`)));
 
+// optimise json files into output dir
 task("json", () => json());
+// optimise json files with verbose output into output dir
 task("json:verbose", () => json(verbose));
+// copy json files into copy dir
 task("json:copy", () => jsonsrc().pipe(dest(`${destdir}/${copieddir}`)));
 
+// optimise mcmeta files into output dir
 task("mcmeta", () => mcmeta());
+// optimise mcmeta files with verbose output into output dir
 task("mcmeta:verbose", () => mcmeta(verbose));
+// copy mcmeta files into copy dir
 task("mcmeta:copy", () => mcmetasrc().pipe(dest(`${destdir}/${copieddir}`)));
 
+// no optimisation available yet, copies ogg files into output dir
 task("ogg", () => ogg());
+// no optimisation or verbose output available yet, copies ogg files into output dir
 task("ogg:verbose", () => ogg(verbose));
+// copy ogg files into copy dir
 task("ogg:copy", () => oggsrc().pipe(dest(`${destdir}/${copieddir}`)));
 
+// copy license file(s) into output dir
 task("copylicense", () => src(`${srcdir}/**/${license}`).pipe(dest(`${destdir}/${optimiseddir}`)));
+// copy license file(s) into copy dir
 task("copylicense:copy", () => src(`${srcdir}/**/${license}`).pipe(dest(`${destdir}/${copieddir}`)));
 
+// deletes copy dir
 task("clean:copy", () => del(`${destdir}/${copieddir}`));
+// deletes output dir
 task("clean:optimise", () => del(`${destdir}/${optimiseddir}`));
+// deletes all output (copy and output dir)
 task("clean", () => del(destdir));
 
+// runs optimisation, puts files into output dir
 task("optimise", series("clean:optimise", parallel("png", "json", "mcmeta", "ogg", "copylicense")));
+// runs optimisation with verbose output, puts files into output dir
 task("optimise:verbose", series("clean:optimise", parallel("png:verbose", "json:verbose", "mcmeta:verbose", "ogg:verbose", "copylicense")));
+// copies files that would be optimised into copy dir
 task("copy", series("clean:copy", parallel("png:copy", "json:copy", "mcmeta:copy", "ogg:copy", "copylicense:copy")));
 
+// runs optimise and copy tasks, so you can easily compare the filesizes and other stuff
 task("compare", series("clean", parallel("copy", "optimise")));
