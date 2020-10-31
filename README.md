fada5f9a554eeb4e178defe8900e4a005b1fcb09
Alexander Liu <liujalexander84@gmail.com>
2020-10-30 18:28:28 -0700

    better readme, more work on some gulp tasks

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # hii
 
-this is a thing idk, it optimises png, json, and mcmeta files (or you can choose not to optimise the text files)
-use: go into the project directory (the directory that has the package.json in it), and create a folder called `original`. inside this folder, put your resourcepack files (not zip) into it. then open up a terminal, cd into the project directory (not original folder), and run `npm start`. the output files will be put into a folder called `optimised`
+this is a thing idk, it copies necessary resourcepack png, json, ogg, and mcmeta files and optimises them if possible to save storage space (or you can choose not to optimise the text files soon™). this thing requires `node` and [`pnpm`](https://pnpm.js.org/).
+
+## installing pnpm
+
+if you have npm installed (you should, it comes with node by default), you can run `npm i -g pnpm` to install it.
+
+## setup
+
+1. `git clone` and `cd` into the dir (standard stuff)
+2. `pnpm i` to install dependencies (still standard (npm) stuff)
+3. put your resourcepack files inside a folder called `original` (not the zip file, put the loose files)
+4. `pnpm run optimise` to optimise/compress your resourcepack
+5. `pnpm run optimise:verbose` to run with verbose output
+6. `pnpx gulp --tasks` to list available tasks
+7. `pnpx gulp <taskname>` to run a task (where `<taskname>` is the name of the task)
+8. see the gulpfile (`gulpfile.js`) for more info on what happens
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,61 @@
-const { task, src, dest, parallel } =require("gulp");
-const jsonmin =require("gulp-jsonmin");
-const imagemin =require("gulp-imagemin");
+"use strict";
 
-task("imagemin", () => src("original/**/*.png").pipe(imagemin()).pipe(dest("optimised")));
-task("imagemin:verbose", () => src("original/**/*.png").pipe(imagemin({ verbose: true })).pipe(dest("optimised")));
+/** the options object passed to the gulp plugins to enable verbose logging */
+const verbose = { verbose: true };
+/** source directory */
+const srcdir = "original";
+/** destination directory */
+const destdir = "out";
+/** optimised directory name */
+const optimiseddir = "optimised";
+/** copied directory name */
+const copieddir = "copied";
+/** name of the license file */
+const license = "LICENSE";
 
-task("jsonmin", () => src(["original/**/*.json", "original/**/*.mcmeta"]).pipe(jsonmin()).pipe(dest("optimised")));
-task("jsonmin:verbose", () => src(["original/**/*.json", "original/**/*.mcmeta"]).pipe(jsonmin({ verbose: true })).pipe(dest("optimised")));
+// npm imports and stuff
+const { task, src, dest, parallel, series } = require("gulp");
+const jsonmin = require("gulp-jsonmin");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
 
-task("copylicense", () => src("LICENSE").pipe(dest("optimised")));
+// rest is stuff and stuff
+const pngsrc = () => src(`${srcdir}/**/*.png`);
+const jsonsrc = () => src(`${srcdir}/**/*.json`);
+const mcmetasrc = () => src(`${srcdir}/**/*.mcmeta`);
+const oggsrc = () => src(`${srcdir}/**/*.ogg`);
 
-exports.optimise_verbose = parallel("imagemin:verbose", "jsonmin:verbose", "copylicense");
-exports.optimise = parallel("imagemin", "jsonmin", "copylicense");
+const png = (opts) => pngsrc().pipe(imagemin(opts)).pipe(dest(`${destdir}/${optimiseddir}`));
+const json = (opts) => jsonsrc().pipe(jsonmin(opts)).pipe(dest(`${destdir}/${optimiseddir}`));
+const mcmeta = (opts) => mcmetasrc().pipe(jsonmin(opts)).pipe(dest(`${destdir}/${optimiseddir}`));
+const ogg = (opts) => oggsrc().pipe(dest(`${destdir}/${optimiseddir}`));
+
+
+task("png", () => png());
+task("png:verbose", () => png(verbose));
+task("png:copy", () => pngsrc().pipe(dest(`${destdir}/${copieddir}`)));
+
+task("json", () => json());
+task("json:verbose", () => json(verbose));
+task("json:copy", () => jsonsrc().pipe(dest(`${destdir}/${copieddir}`)));
+
+task("mcmeta", () => mcmeta());
+task("mcmeta:verbose", () => mcmeta(verbose));
+task("mcmeta:copy", () => mcmetasrc().pipe(dest(`${destdir}/${copieddir}`)));
+
+task("ogg", () => ogg());
+task("ogg:verbose", () => ogg(verbose));
+task("ogg:copy", () => oggsrc().pipe(dest(`${destdir}/${copieddir}`)));
+
+task("copylicense", () => src(`${srcdir}/**/${license}`).pipe(dest(`${destdir}/${optimiseddir}`)));
+task("copylicense:copy", () => src(`${srcdir}/**/${license}`).pipe(dest(`${destdir}/${copieddir}`)));
+
+task("clean:copy", () => del(`${destdir}/${copieddir}`));
+task("clean:optimise", () => del(`${destdir}/${optimiseddir}`));
+task("clean", () => del(destdir));
+
+task("optimise", series("clean:optimise", parallel("png", "json", "mcmeta", "ogg", "copylicense")));
+task("optimise:verbose", series("clean:optimise", parallel("png:verbose", "json:verbose", "mcmeta:verbose", "ogg:verbose", "copylicense")));
+task("copy", series("clean:copy", parallel("png:copy", "json:copy", "mcmeta:copy", "ogg:copy", "copylicense:copy")));
+
+task("compare", series("clean", parallel("copy", "optimise")));
